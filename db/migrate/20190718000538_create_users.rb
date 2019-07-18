a73021febfa3681ca057cea13655731ddc3f3242
Karly Mareka <karly.mareka@gmail.com>
2019-07-18 01:30:23 +0000

    Automatically backed up by Learn

--- db/migrate/20190718000538_create_users.rb
+++ db/migrate/20190718000538_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
      create_table :users do |t|
       t.string :name 
-      t.integer :email 
+      t.string :email 
       t.string :password 
     end 
   end
